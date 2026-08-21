e65c832a94358608f6fee834fdfa3ad85e59ff6b
Test <test@test>
2026-08-21 08:34:12 +0000

    init

--- docs/theme.config.jsx
+++
@@ -1,6 +0,0 @@
-
-export default {
-  logo: <span>Verdict Core</span>,
-  project: { link: 'https://github.com/verdict/verdict-core' },
-  docsRepositoryBase: 'https://github.com/verdict/verdict-core/tree/main/docs',
-}
--- tests/test_swarm.py
+++
@@ -1,9 +0,0 @@
-from verdict.swarm import initiate_swarm
-
-
-def test_initiate_swarm():
-    class DummyIntelligence:
-        pass
-
-    res = initiate_swarm("build UI", DummyIntelligence())  # type: ignore
-    assert res["ready_for_architect"] is True
